24dd2526cb9ee308e6de49b000ac060d9655f04e
Nick <nickelphelps@gmail.com>
2023-12-19 11:29:53 -0800

    Delete Emulsion Analysis directory, to be replaced with updated folder

--- Emulsion Analysis/remove_suffix.py
+++
@@ -1,7 +0,0 @@
-
-def remove_suffix(input_string, suffix: str):
-    if input_string.endswith(suffix):
-        result_string = input_string[:-4]
-        return result_string
-    else:
-        return input_string
